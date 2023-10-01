eee10440f5c60cf819c5c1acae6c897273c4c05e
canneron <mcgreevycameron@gmail.com>
2023-10-01 19:53:23 +0100

    Adding 2 servers that can chat to eachother via UDP

---
+++ server.cpp
@@ -0,0 +1,104 @@
+// Server side implementation of UDP client-server model
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <thread>
+
+#define MAXLINE 1024
+using namespace std;
+
+void listener() {
+	int sockfd;
+	char buffer[MAXLINE];
+	const char *hello = "Hello from server";
+	struct sockaddr_in servaddr, cliaddr;
+	cout << "Listening..." << endl;
+	// Creating socket file descriptor
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
+	
+	memset(&servaddr, 0, sizeof(servaddr));
+	memset(&cliaddr, 0, sizeof(cliaddr));
+	
+	// Filling server information
+	servaddr.sin_family = AF_INET; // IPv4
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(5001);
+	
+	// Bind the socket with the server address
+	if ( bind(sockfd, (const struct sockaddr *)&servaddr,
+			sizeof(servaddr)) < 0 )
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	int n;
+	socklen_t len;
+	len = sizeof(cliaddr);
+	n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+				MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+				&len);
+	buffer[n] = '\0';
+	printf("Client : %s\n", buffer);
+
+}
+
+// Driver code
+int main() {
+	int sockfd;
+	char buffer[MAXLINE];
+	const char *hello = "Hello from server";
+	struct sockaddr_in servaddr, cliaddr;
+	
+	thread listenT(listener);
+
+	// Creating socket file descriptor
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
+	
+	memset(&servaddr, 0, sizeof(servaddr));
+	memset(&cliaddr, 0, sizeof(cliaddr));
+	
+	// Filling server information
+	servaddr.sin_family = AF_INET; // IPv4
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(0);
+
+	cliaddr.sin_family = AF_INET; // IPv4
+	cliaddr.sin_addr.s_addr = INADDR_ANY;
+	cliaddr.sin_port = htons(5002);
+	
+	// Bind the socket with the server address
+	if ( bind(sockfd, (const struct sockaddr *)&servaddr,
+			sizeof(servaddr)) < 0 )
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	
+	socklen_t len;
+	int n;
+
+	len = sizeof(cliaddr); //len is value/result
+	string input;
+	char* chinput = new char[input.length()];
+    getline(std::cin, input);
+	strcpy(chinput, input.c_str());
+	cout << "Sending..." << endl;
+
+	sendto(sockfd, (const char *)chinput, strlen(hello),
+		MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
+			len);
+	std::cout<<"Hello message sent."<<std::endl;
+	listenT.join();
+	return 0;
+}
